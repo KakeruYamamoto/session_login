b7caf1d18b0c52673d394e4c74347de0aa1c284d
kakeru <okinawan.kkr0713@icloud.com>
2019-10-18 13:39:21 +0900

    アクション追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,23 @@
 class UsersController < ApplicationController
+
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+    #  redirect #保存が成功した場合の処理
+    else
+      render 'new'
+    end  
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)#userキーのそれぞれのカラム
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
-  validates :name,  presence: true, length: { maximum: 30 }
+  validates :name,  presence: true, length: { maximum: 30 }# nameなどはテーブルカラム
   validates :email, presence: true, length: { maximum: 255 },
             format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
             # uniqueness: true
   before_validation { email.downcase! } #強制的に小文字に変える。
-  has_secure_password  #スペルミスにはきよつける
+  has_secure_password
+  #このメソッドはパスワードをハッシュ化する。そしてその変換したものをDBのpassword_digestというカラムに保存する。
+  #そして、新たなめっソッドが使用できる。authenticate   #スペルミスにはきよつける
   validates :password, presence: true, length: { minimum: 6 }#空の値禁止、文字制限
   # validates :name, presence: true,length: {maxium: 30}
   # validates :email, presence: true,length: {maxium: 255}
